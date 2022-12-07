d16e530406714c34a70cf1090ec38e194774bebf
SmallPeenwow <bowdencamryn@gmail.com>
2022-12-07 09:21:12 +0200

    Footer Page Done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import SocialMediaIcons from './SocialMediaIcons';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className='h-64 bg-red pt-10'>
+			<div className='w-5/6 mx-auto'>
+				<SocialMediaIcons />
+				<div className='md:flex justify-center md:justify-between text-center'>
+					<p className='uppercase font-playfair font-semibold text-2xl text-yellow'>Camryn Bowden</p>
+					<p className='font-playfair text-sm text-yellow'>©2022 Bowden. All Rights Reserved.</p>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
